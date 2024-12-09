2a0b19f9d4ad6c13881aca8302bbccc7d56f1ac8
Youssef57-T <125463871+Youssef57-T@users.noreply.github.com>
2024-12-09 14:34:04 +0200

    yarb

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,29 +6,32 @@ import {userRoutes} from './routes/userRoutes.js'
 const app = express();
 
 
-const corsOptions = {
-  origin: 'http://localhost:3000', 
-  credentials: true 
-};
+// const corsOptions = {
+//   origin: 'http://localhost:3000', 
+//   credentials: true 
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors())
 app.use(express.json());
 
 
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
 
-app.use(session({
-  secret: '1e3w7s8ab9sevs', 
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false } 
-}));
+// app.use(session({
+//   secret: '1e3w7s8ab9sevs', 
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: { secure: false } 
+// }));
 
 app.use('/api/products', productRoutes);
 app.use('/api/cart' , cartRoutes);
 app.use('/api/users', userRoutes);
-
 
 app.get('/profile', (req, res) => {
   if (req.session.existingUser) {
