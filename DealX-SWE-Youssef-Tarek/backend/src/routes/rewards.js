d0c40e3843f3ad18d461b4c8a020490ce693120e
Youssef57-T <125463871+Youssef57-T@users.noreply.github.com>
2024-12-25 19:48:32 +0200

    Delete DealX-SWE-Youssef-Tarek directory

--- DealX-SWE-Youssef-Tarek/backend/src/routes/rewards.js
+++
@@ -1,9 +0,0 @@
-// src/routes/rewardRoutes.js
-import express from 'express';
-import { getAllRewards } from '../controllers/rewardController.js';
-
-const RewardRouter = express.Router();
-
-RewardRouter.get('/', getAllRewards);
-
-export default RewardRouter;
--- DealX-SWE-Youssef-Tarek/backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {createUser, getUser} from '../controllers/userController.js';
-
-const userRoutes = express.Router();
-
-userRoutes.post('/', getUser); 
-userRoutes.post('/signup', createUser); 
-
-export {userRoutes};
